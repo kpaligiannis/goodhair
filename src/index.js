343a15d94e809cac4f2789cc168010a894035a34
Konstantinos Paligiannis <konstantinos.paligiannis@gmail.com>
2025-02-01 15:28:27 +0000

    init

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import './style.css';
+
+console.log('Application loaded');
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('menu-toggle');
+    if (menuToggle) {
+        menuToggle.addEventListener('click', function() {
+            const menuModal = document.getElementById('menu-modal');
+            if (menuModal) {
+                menuModal.classList.toggle('hidden');
+                menuModal.classList.toggle('visible');
+            }
+        });
+    }
+
+    const closeButton = document.getElementById('close');
+    if (closeButton) {
+        closeButton.addEventListener('click', function() {
+            const menuModal = document.getElementById('menu-modal');
+            if (menuModal) {
+                menuModal.classList.add('hidden');
+                menuModal.classList.remove('visible');
+            }
+        });
+    }
+
+    const acc = document.getElementsByClassName("accordion");
+    let i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight){
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
+});
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/people.html',
+            filename: 'people.html',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/services.html',
+            filename: 'services.html',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/products.html',
+            filename: 'products.html',
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(woff|woff2|ttf|otf|eot)$/,
+                type: 'asset/resource',
+                dependency: { not: ['url'] },
+            }
+        ]
+    }
+};
